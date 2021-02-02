b6ad5ebf12ca40e5b59b78ed8b512175dda59b53
murphywuwu <murphywuwu@gmail.com>
2021-02-02 22:27:35 +0800

    添加批量输入应用id功能

--- app-store/splider.py
+++ app-store/splider.py
@@ -1,101 +1,132 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
 import requests
-import urllib.request
-import re
-import xlsxwriter
+# import urllib.request
+# import re
+# import xlsxwriter
+import openpyxl
 import json
+import inquirer
+# import os
+import time
 import os
 
+apps = {}
 
-def getHTMLText(url):
-    response = urllib.request.urlopen(url)
-    myjson = json.loads(response.read().decode())
+def SearchAppId(app): 
+  url = "http://itunes.apple.com/search?term=" + app + "&entity=software"
+  r = requests.get(url)
+  html = r.content
+  html_doc = str(html, 'utf-8')
+  data = json.loads(html_doc)
+  resultCount = data['resultCount']
+  results = data['results']
+  print(app + ' Find ' + str(resultCount) + 'result(s)')
 
-    return myjson
+  for i in range(resultCount):
+    app_name = results[i]['trackName']
+    app_id = results[i]['trackId']
+    apps[app_id] = app_name
+    print('name: ' + app_name, 'id: ' + str(app_id))
 
-    # try:
-    #     r = requests.get(url)
-    #     r.raise_for_status()
-    #     r.encoding = r.apparent_encoding
-    #     return r.text
-    # except:
-    #     return ''
+def SaveContent(wb, ws, app_id, app_name, row):
+    # row = 2
 
+    for j in range(1, 11): # 只能爬取前10页
+        url = 'https://itunes.apple.com/rss/customerreviews/page=' + str(j) + '/id=' + str(app_id) + '/sortby=mostrecent/json?l=en&&cc=cn'
+        print('当前地址：' + url)
+       
+        r = requests.get(url)
+
+        if r.status_code == 200:
+          html = r.content
+          html_doc = str(html, 'utf-8')
+          data = json.loads(html_doc)['feed'].get('entry') or []
+          for i in data:
+            name = i['author']['name']['label']
+            rate = i['im:rating']['label']
+            user_id = i['id']['label']
+            content = i['content']['label']
+            version = i['im:version']['label']
+
+            ws.cell(row=row, column=1, value=app_id)
+            ws.cell(row=row, column=2, value=app_name)
+            ws.cell(row=row, column=3, value=name)
+            ws.cell(row=row, column=4, value=rate)
+            ws.cell(row=row, column=5, value=user_id)
+            ws.cell(row=row, column=6, value=content)
+            ws.cell(row=row, column=7, value=version)
+
+            row = row + 1
+            print(name, rate, user_id,  content)
+
+        # 每一页爬取延迟2秒，以防过于频繁  
+        time.sleep(2)
+    wb.save('app_store.xlsx')
+def startFetch(wb, ws):
+  
+  # name_list = wb.sheetnames
+  is_continue = 'yes'
+  ids = []
+
+  while is_continue == 'yes':
+    app_id = input("input app's id: \n")
+    
+    if not int(app_id) in apps:
+      break
+    
+
+    app_name = apps[int(app_id)]
+    print('应用名称: ' + app_name)
+
+    ids.append({ 'app_id': app_id, 'app_name': app_name })
+    questions = [
+      inquirer.List('continue',
+                    message="需要继续输入appId吗",
+                    choices=['yes', 'no'],
+      ),
+    ]
+    answers = inquirer.prompt(questions)
+    is_continue = answers['continue']
+  
+  row = ws.max_row
+
+  for i in range(len(ids)):
+    SaveContent(wb, ws, ids[i]['app_id'], ids[i]['app_name'] , row+1)
 
 def main():
 
-    appid = input("请输入应用id号:")
-    appName = input("请输入应用名称:")
+    # appid = input("请输入应用id号:")
+    name = input("请输入应用名称:")
+    SearchAppId(name)
+    
+    # if not os.path.exists(appid):
+        # os.system('mkdir ' + appid)
 
-    if not os.path.exists(appid):
-        os.system('mkdir ' + appid)
+    if os.path.exists('app_store.xlsx'):
+      wb = openpyxl.load_workbook('app_store.xlsx')
+      ws = wb['comment']
 
-    workbook = xlsxwriter.Workbook(appid + '/' + appName + '_comments.xlsx')
-    worksheet = workbook.add_worksheet()
-    format = workbook.add_format()
-    format.set_border(1)
-    format.set_border(1)
-    format_title = workbook.add_format()
-    format_title.set_border(1)
-    format_title.set_bg_color('#cccccc')
-    format_title.set_align('left')
-    format_title.set_bold()
-    title = ['作者', '标题', '评论内容', '版本', '评级', '投票']
+      startFetch(wb, ws)
+    else:
+      # Workbook init
+      wb = openpyxl.Workbook()
+      ws = wb.active
+      ws.title = 'comment'
 
-    # 设置单元格宽度
-    worksheet.set_column(0, 0, 30)
-    worksheet.set_column(1, 1, 40)
-    worksheet.set_column(2, 2, 100)
-    worksheet.set_column(3, 3, 10)
-    worksheet.set_column(4, 4, 10)
-    worksheet.set_column(5, 5, 10)
+      ws.cell(row=1, column=1, value="APP ID")
+      ws.cell(row=1, column=2, value="APP 名称")
+      ws.cell(row=1, column=3, value="昵称")
+      ws.cell(row=1, column=4, value="评分")
+      ws.cell(row=1, column=5, value="用户id")
+      ws.cell(row=1, column=6, value="评论")
+      ws.cell(row=1, column=7, value="版本")
 
-    worksheet.write_row('A1', title, format_title)
+      startFetch(wb, ws)
 
-    count = 0
-
-    total = 10
-    totalCount = 0
-    for n in range(total):
-        url = 'https://itunes.apple.com/rss/customerreviews/page=' + \
-            str(n+1) + '/id=' + str(appid) + \
-            '/sortby=mostrecent/json?l=en&&cc=kh'
-
-        print('当前地址：' + url)
-
-        jsonText = getHTMLText(url)
-
-        fileName = appid + '/' + str(n+1) + '.json'
-
-        data_feed = jsonText['feed']
-        entry = data_feed['entry']
-        for i in range(len(entry)):
-            value = entry[i]
-            fixedIndex = i + 1
-            startRow = totalCount + 1
-            worksheet.write(
-                startRow, 0, value['author']['name']['label'], format)
-            worksheet.write(startRow, 1, value['title']['label'], format)
-            worksheet.write(startRow, 2, value['content']['label'], format)
-            worksheet.write(
-                startRow, 3, value['im:version']['label'], format)
-            worksheet.write(
-                startRow, 4, value['im:rating']['label'], format)
-            worksheet.write(
-                startRow, 5, value['im:rating']['label'], format)
-            totalCount = totalCount + 1
-
-        with open(fileName, 'w') as file:
-            file.write(json.dumps(jsonText, sort_keys=True,
-                                  indent=4, ensure_ascii=False))
-
-        count = count + 1
-        print("当前进度: {:.2f}%".format(count * 100 / total), end="\n\n")
-
-    workbook.close()
-
+    
+    print('Done!')
 
 if __name__ == '__main__':
     main()
